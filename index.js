505de8a092129531aad21e92dd7e1c92f565b99b
Alexander Horner <147552108+bbdrip@users.noreply.github.com>
2023-12-10 23:09:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const love = 'LOVE';
-const power = 'POWER'
-const pain = 'PAIN'
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
 
-const alphabet = love + power + pain;
+const alphabet = katakana + latin + nums;
 
 const fontSize = 16;
 const columns = canvas.width/fontSize;
@@ -23,7 +23,7 @@ const draw = () => {
 	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
-	context.fillStyle = '#F5F5F5';
+	context.fillStyle = '#0F0';
 	context.font = fontSize + 'px monospace';
 
 	for(let i = 0; i < rainDrops.length; i++)
