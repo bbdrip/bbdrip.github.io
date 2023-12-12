3be4da0eb431fa0396f57d1991339b5a0c46711e
Alexander Horner <147552108+bbdrip@users.noreply.github.com>
2023-12-12 15:02:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ for( let x = 0; x < columns; x++ ) {
 
 const draw = () => {  
   
-	context.fillStyle = rgba(0, 0, 0, 0.05);
+	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
 	context.fillStyle = '#ff0000';
