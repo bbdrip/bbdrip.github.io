9843077b3abf797c7ca8e99a9d509b5239ad861a
Alexander Horner <147552108+bbdrip@users.noreply.github.com>
2023-12-12 14:35:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const love = 'LOVE';
 const power = 'POWER';
 const pain = 'PAIN';
 
+const backgroundImage = new Image();
+    backgroundImage.src = 'images/triptych.jpg';
+
 const alphabet = love + power + pain;
 
 const fontSize = 16;
@@ -23,15 +26,11 @@ for( let x = 0; x < columns; x++ ) {
 
 const draw = () => {  
   
-	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+	context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
 	context.fillStyle = '#ff0000';
 	context.font = fontSize + 'px monospace';
-    context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-    const backgroundImage = new Image();
-    backgroundImage.src = 'images/triptych.jpg';
-
 
 	for(let i = 0; i < rainDrops.length; i++)
 	{
