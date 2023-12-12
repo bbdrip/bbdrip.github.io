35dc34391f784e4e7ac28a0b12ddc74291ed308c
Alexander Horner <147552108+bbdrip@users.noreply.github.com>
2023-12-12 13:49:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const draw = () => {
 	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
-	context.fillStyle = '#F5F5F5';
+	context.fillStyle = '#ff0000';
 	context.font = fontSize + 'px monospace';
 
 	for(let i = 0; i < rainDrops.length; i++)
