08d61f89128c604ffee587bf3952ec814e2c81fc
Alexander Horner <147552108+bbdrip@users.noreply.github.com>
2023-12-19 15:26:19 -0500

    reset to original working

--- index.js
+++ index.js
@@ -5,24 +5,6 @@ setTimeout(function()  {
 	document.getElementById('overlay-text').style.display = 'block';
 }, 6000);
 
-
-$(".mute").click(function(event) {
-
-	document.getElementById("debussy").muted = true;
-	$(this).hide();
-	$(".unmute").show();
-
-});
-
-$(".unmute").click(function(event) {
-
-	document.getElementById("debussy").muted = false;
-	$(this).hide();
-	$(".mute").show();
-
-});
-
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
